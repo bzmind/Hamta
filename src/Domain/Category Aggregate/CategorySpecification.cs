a8b3a7efcc0003304a5bb22aceb817dd5f9f3772
smh6 <smh.gamism@gmail.com>
2022-02-23 19:59:07 +0330

    Completed Category Aggregate

---
+++ src/Domain/Category Aggregate/CategorySpecification.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Shared.BaseClasses;
+using Domain.Shared.Exceptions;
+
+namespace Domain.Category_Aggregate;
+
+public class CategorySpecification : BaseEntity
+{
+    public long CategoryId { get; private set; }
+    public string Title { get; private set; }
+
+    public CategorySpecification(long categoryId, string title)
+    {
+        CategoryId = categoryId;
+        Title = title;
+    }
+
+    public void Edit(string title)
+    {
+        NullOrEmptyDataDomainException.CheckString(title, nameof(title));
+        Title = title;
+    }
+}
---
+++ src/Domain/Category Aggregate/Services/ICategoryService.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Category_Aggregate.Services;
+
+public interface ICategoryService
+{
+    bool DoesCategoryExist(long categoryId);
+}
