e323c9842dceb4ad700ae8328b114d8f17ef1263
smh6 <smh.gamism@gmail.com>
2022-03-02 14:00:59 +0330

    Updated product aggregate

---
+++ src/Domain/Product Aggregate/ProductAnswer.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Shared.BaseClasses;
+using Domain.Shared.Exceptions;
+
+namespace Domain.Product_Aggregate;
+
+public class ProductAnswer : BaseEntity
+{
+    public long ProductId { get; private set; }
+    public long CustomerId { get; private set; }
+    public long ParentId { get; private set; }
+    public string ParentDescription { get; private set; }
+    public string Description { get; private set; }
+
+    public ProductAnswer(long productId, long customerId, long parentId,
+        string parentDescription,string description)
+    {
+        NullOrEmptyDataDomainException.CheckString(parentDescription, nameof(parentDescription));
+        NullOrEmptyDataDomainException.CheckString(description, nameof(description));
+        ProductId = productId;
+        CustomerId = customerId;
+        ParentId = parentId;
+        ParentDescription = parentDescription;
+        Description = description;
+    }
+}
---
+++ src/Domain/Product Aggregate/ProductQuestion.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Shared.BaseClasses;
+using Domain.Shared.Exceptions;
+
+namespace Domain.Product_Aggregate;
+
+public class ProductQuestion : BaseAggregateRoot
+{
+    public long ProductId { get; private set; }
+    public long CustomerId { get; private set; }
+    public string Description { get; private set; }
+    public List<ProductAnswer> Answers { get; private set; }
+
+    public ProductQuestion(long productId, long customerId, string description, List<ProductAnswer> answers)
+    {
+        NullOrEmptyDataDomainException.CheckString(description, nameof(description));
+        ProductId = productId;
+        CustomerId = customerId;
+        Description = description;
+        Answers = answers;
+    }
+
+    public void AddAnswer(ProductAnswer answer)
+    {
+        Answers.Add(answer);
+    }
+
+    public void RemoveAnswer(long answerId)
+    {
+        var answer = Answers.FirstOrDefault(a => a.Id == answerId);
+
+        if (answer == null)
+            throw new NullOrEmptyDataDomainException($"No such answer was found for this question: {answerId}");
+
+        Answers.Remove(answer);
+    }
+}
