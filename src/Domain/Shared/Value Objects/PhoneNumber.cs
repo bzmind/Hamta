b0b6e09f72a44178836bd2db7519622f2eaba59a
smh6 <smh.gamism@gmail.com>
2022-03-02 15:36:03 +0330

    Completed PhoneNumber value object

--- src/Domain/Shared/Value Objects/PhoneNumber.cs
+++ src/Domain/Shared/Value Objects/PhoneNumber.cs
@@ -1,8 +1,18 @@
 ﻿using Domain.Shared.BaseClasses;
+using Domain.Shared.Exceptions;
 
 namespace Domain.Shared.Value_Objects;
 
 public class PhoneNumber : BaseValueObject
 {
-    
+    public string Value { get; private set; }
+
+    public PhoneNumber(string phoneNumber)
+    {
+        NullOrEmptyDataDomainException.CheckString(phoneNumber, nameof(phoneNumber));
+        if (phoneNumber.Length is > 11 or < 11)
+            throw new InvalidDataDomainException("Phone number cannot be greater or less than 11 characters");
+
+        Value = phoneNumber;
+    }
 }
