733150f066e50746cc95b7bef42503313510d5b9
smh6 <smh.gamism@gmail.com>
2022-09-07 19:12:44 +0430

    Added all static files needed for project

---
+++ src/Shop/Shop.Presentation/Shop.UI/wwwroot/admin/js/scripts/forms/form-repeater.js
@@ -0,0 +1,18 @@
+/*=========================================================================================
+    File Name: Form-Repeater.js
+    Description: form repeater page specific js
+==========================================================================================*/
+
+$(document).ready(function () {
+	// form repeater jquery
+	$(".file-repeater, .contact-repeater, .repeater-default, .repeater").repeater({
+		show: function () {
+			$(this).slideDown();
+		},
+		hide: function (deleteElement) {
+			if (confirm("آیا از حذف این آیتم مطمئن هستید؟")) {
+				$(this).slideUp(deleteElement);
+			}
+		}
+  });
+});
